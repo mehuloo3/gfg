73c78548916bc3b6e45332a7a7bfc71fb7b7cb05
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-05-19 11:03:32 +0530

    Updated Solution - GfG to GitHub

--- Easy/Find the closest number/find-the-closest-number.cpp
+++ Easy/Find the closest number/find-the-closest-number.cpp
@@ -10,57 +10,18 @@ class Solution{
     int findClosest( int n, int k,int arr[]) 
     { 
         // Complete the function
-        int ans=arr[0];
+        int a=arr[0];
         int diff=abs(arr[0]-k);
         for(int i=1;i<n;i++)
         {
             int current=abs(arr[i]-k);
-            if(current<diff || (current == diff && arr[i]>ans))
+            if(current<diff || (current == diff && arr[i]>a))
             {
                 diff=current;
-                ans=arr[i];
+                a=arr[i];
             }
         }
-        return ans;
-        
-        
-        
-        // int start=0;
-        // int end=n-1;
-        // int maxi=INT_MAX;
-        // int ans=0;
-        // while(start<=end)
-        // {
-        //     int mid=start+(start-end)/2;
-        //     int x=abs(arr[mid]-k);
-        //     if(abs(ans-k)>x) 
-        //     ans=arr[mid];
-        //     if(abs(ans-k)==x)
-        //     ans=max(ans,arr[mid]);
-        //     if(arr[mid]>k)
-        //     end=mid-1;
-        //     else
-        //     start=mid+1;
-        //     if(x<=maxi & (x<maxi || arr[mid]>ans))
-        //     {
-        //         ans=arr[mid];
-        //         maxi=x;
-        //     }
-        //  if(arr[mid]==k)
-        //     {
-        //         return ans;
-        //     }
-        //  if(arr[mid]>k)
-        //     {
-        //         end=mid-1;
-        //     }
-        //     else
-        //     {
-        //         end=mid+1;
-        //     }
-        // }
-        // }
-        // return ans;
+        return a;
     } 
 };
 
