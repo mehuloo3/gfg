ec2d6b76279af0a63c35f22b241dace0bb1d98fc
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-06-12 15:05:43 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count numbers containing 4/count-numbers-containing-4.js
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString =
+        inputString.trim().split("\n").map(string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+
+        let n = parseInt(readLine());
+
+        let obj = new Solution();
+        let res = obj.countNumberswith4(n);
+        console.log(res);
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    /**
+    * @param number n
+
+    * @returns number
+    */
+    countNumberswith4(n) {
+        // code here
+        
+        let count=0;
+        for(let i=4;i<=n;i++)
+        {
+            let num=i.toString();
+            if(num.includes(4))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
