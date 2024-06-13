f235f38faa2e5b618aabdcaf4a959f46216b1c44
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-06-13 19:21:06 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Jump Game/jump-game.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int canReach(int A[], int N) {
+        // code here
+        
+        int i=0,j=0;
+        while(i<N && i<=j)
+        {
+            j=max(j,A[i]+i);
+            i++;
+        }
+        if(i==N)
+        return 1;
+        else
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        
+        int A[N];
+        
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.canReach(A,N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
