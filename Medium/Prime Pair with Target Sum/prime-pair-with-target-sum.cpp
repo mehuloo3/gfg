f00d1f434d9758e6f2b2e9824d72ebf6d198ae7e
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-06-18 13:07:11 +0530

    Updated Solution - GfG to GitHub

--- Medium/Prime Pair with Target Sum/prime-pair-with-target-sum.cpp
+++ Medium/Prime Pair with Target Sum/prime-pair-with-target-sum.cpp
@@ -26,17 +26,26 @@ class Array {
 class Solution {
   public:
   bool isPrime(int n){
-        if(n == 1)
-            return false;
-        if(n==2 || n ==3)
-            return true;
-        if(n%2 == 0 || n%3 == 0)
-            return false;
-        for(int i=5; i*i<=n; i+=6){
-            if(n%i==0 || n%(i+2)==0)
-                return false;
-        }
-        return true;
+        // if(n==1)
+        //     return false;
+        // if(n==2||n==3)
+        //     return true;
+        // if(n%2==0||n%3==0)
+        //     return false;
+        // for(int i=5;i*i<=n;i+=6){
+        //     if(n%i==0 || n%(i+2)==0)
+        //         return false;
+        // }
+        // return true;
+        if (n <= 1) 
+        return false; 
+  
+    // Check from 2 to square root of n 
+    for (int i = 2; i <= sqrt(n); i++) 
+        if (n % i == 0) 
+            return false; 
+  
+    return true;
     }
     vector<int> getPrimes(int n) {
         // code here
