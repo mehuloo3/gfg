985b4f07c6075e896afe378239fce789a2ba6d72
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-06-22 10:23:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Euler Totient Sum and Divisors/euler-totient-sum-and-divisors.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+public:
+    int phiSum(int N){
+        // code here
+        return N;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.phiSum(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
