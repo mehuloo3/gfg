b42dcb311e1dbcbf673978280233378a34c44c98
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-06-24 18:53:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Plus One/plus-one.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    vector<int> increment(vector<int> arr ,int N) {
+        // code here
+        
+        for(int i=N-1;i>=0;i--)
+        {
+            arr[i]++;
+            arr[i]=arr[i]%10;
+            
+            if(arr[i]!=0)
+            {
+                return arr;
+            }
+        }
+        arr.insert(arr.begin(),1);
+       return arr; 
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        char c;
+        
+        cin>>N;
+        vector<int> arr(N);
+        
+        for(int i=0 ; i<N ; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        vector<int> res = ob.increment(arr,N);
+        for(int i: res)
+            cout<<i<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
