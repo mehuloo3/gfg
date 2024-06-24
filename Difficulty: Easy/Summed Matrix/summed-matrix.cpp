06a472c0211952ab6b2e474a38ee325fad57e1ce
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-06-24 17:01:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Summed Matrix/summed-matrix.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long sumMatrix(long long n, long long q) {
+        // code here
+        // int count=0;
+        // for(int i=1;i<=n;i++)
+        // {
+        //     for(int j=1;j<=n;j++)
+        //     {
+        //         if((i+j)==q)
+        //         {
+        //             count++;
+        //         }
+        //     }
+        // }
+        // int i=1,j=1;
+        // while(i<=n && j<=n)
+        // {
+        //     if((i+j)==q)
+        //     {
+        //         count++;
+        //     }
+        //     i++;
+        //     j++;
+        // }
+        // return count;
+        
+        if(q>2*n || q<2)
+        {
+            return 0;
+        }
+        else if(q<=n)
+        {
+            return q-1;
+        }
+        else
+        {
+            return 2*n-q+1;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n,q;
+        
+        cin>>n>>q;
+
+        Solution ob;
+        cout << ob.sumMatrix(n,q) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
