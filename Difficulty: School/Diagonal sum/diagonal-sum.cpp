3fd8c243cd74192885742a8537df5726f408c8a3
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-06-27 09:11:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Diagonal sum/diagonal-sum.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    int DiagonalSum(vector<vector<int> >& matrix) {
+        // Code here
+        int sum1=0,sum2=0;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==j){
+                sum1+=matrix[i][j];
+                }
+                if(i+j==n-1){
+                sum2+=matrix[i][j];
+                }
+            }
+        }
+        return sum1+sum2;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>> matrix(n, vector<int>(n,0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> matrix[i][j];
+			}
+		}
+		Solution ob;
+		int ans = ob.DiagonalSum(matrix);
+		cout << ans <<'\n';
+	}
+	return 0;
+}
+// } Driver Code Ends
