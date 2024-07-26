fdf423d1c14ff05e3538d236cbaed439d03ae4cf
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-07-26 16:29:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/K-Pangrams/kpangrams.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    bool kPangram(string str, int k) {
+        // code here
+        vector<int>v(26,0);
+        // int n=.size();
+        int ch=0;
+        for(int i=0;i<str.length();i++)
+        {
+            if(str[i] !=' ')
+            {
+                v[str[i]-'a']=1;
+                ch++;
+            }
+        }
+        int c=0;
+        for(int i=0;i<26;i++)
+        {
+            if(v[i]==0)
+            {
+                c++;
+            }
+        }
+        return (ch>=26) && (c<=k);
+    //     if(ch<26 || c>k) return false;
+        
+    // return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin.ignore();
+        string str;
+        getline(cin, str);
+
+        int k;
+        cin >> k;
+
+        Solution ob;
+        bool a = ob.kPangram(str, k);
+        if (a)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
