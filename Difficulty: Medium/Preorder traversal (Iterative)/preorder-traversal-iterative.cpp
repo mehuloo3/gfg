e0aabccb2214c79d0b832f5e0c86379737dd26fa
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-08-04 10:16:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Preorder traversal (Iterative)/preorder-traversal-iterative.cpp
+++ Difficulty: Medium/Preorder traversal (Iterative)/preorder-traversal-iterative.cpp
@@ -97,7 +97,6 @@ struct Node {
 */
 class Solution{
     public:
-    
     vector<int> preOrder(Node* root)
     {
         //code here
@@ -106,13 +105,17 @@ class Solution{
         vector<int>ans;
         while(!s.empty())
         {
-            Node* temp=s.top();
+            Node *temp=s.top();
             s.pop();
             ans.push_back(temp->data);
             if(temp->right)
-             s.push(temp->right);
+            {
+                s.push(temp->right);
+            }
             if(temp->left)
-             s.push(temp->left);
+            {
+                s.push(temp->left);
+            }
         }
         return ans;
     }
