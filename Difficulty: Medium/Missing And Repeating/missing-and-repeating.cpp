ec7cb0603fc1984ae8b108cf68070d7cc74dca99
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-09-23 10:18:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Missing And Repeating/missing-and-repeating.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> findTwoElement(vector<int>& arr) {
+        // code here
+        unordered_map<int,int>map;
+        vector<int>ans;
+        for(auto i:arr)
+        {
+            map[i]++;
+        }
+        for(auto i:map)
+        { 
+            if(i.second>1)
+            {
+                ans.push_back(i.first);
+            }
+            
+        }
+        
+    int maxi=arr[0];
+    for(int i=0;i<arr.size();i++){
+    
+        if(arr[i]>maxi)
+        {
+            maxi=arr[i];
+        }
+    }
+    int miss;
+      for(int i=1;i<=arr.size();i++){
+            auto it=map.find(i);
+            if(it==map.end()) miss=i;
+        }
+     ans.push_back(miss);
+      return ans;  
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findTwoElement(a);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
