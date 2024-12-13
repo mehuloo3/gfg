6185e5715ac0063569fabe562aa353ca3353715c
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-12-13 13:41:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element in Circular Array/next-greater-element-in-circular-array.cpp
+++ Difficulty: Medium/Next Greater Element in Circular Array/next-greater-element-in-circular-array.cpp
@@ -9,53 +9,20 @@ using namespace std;
 class Solution {
   public:
     vector<long long> nextLargerElement(vector<long long> &arr,int n){
-        // vector<long long>ans(n,-1);
-        // stack<int>st;
-        // for(int i=n-1;i>=n;i--)
-        // {
-        //     while(!st.empty() && arr[st.top()]<arr[i])
-        //     st.pop();
-            
-        //     if(!st.empty())
-        //     ans[i]=arr[st.top()];
-            
-        //     st.push(i);
-        // }
-        // return ans;
-        
-        
-// vector<long long>ans(n,-1);
-//  for(int i=0;i<n;i++)
-//  {
-//     for(int j=i+1;j<n;j++)
-//     {
-//         if(arr[j]>arr[i])
-//         {
-//             ans[i]=arr[j];
-//             break;
-//         }
-//     }
-//  }
- 
-//  return ans;
-
-
- stack<long long> st;
-        st.push(-1);
-        vector<long long> ans(n);
-        int x=2;
-        while(x) {
-            for(int i=n-1;i>=0;i--)
+        vector<long long>ans(n,-1);
+        stack<int>st;
+        for(int i=0;i<2*n;i++)
+        {
+            while(!st.empty() && arr[st.top()]<arr[i%n])
             {
-                while(st.top()!=-1 && st.top()<=arr[i]) st.pop();
-                ans[i]=st.top();
-                st.push(arr[i]);
+            ans[st.top()]=arr[i%n];
+            st.pop();
             }
-            x--;
+            if(i<n){
+            st.push(i);
+            }
         }
         return ans;
-        
-   
     }
 };
 
