6f3261530169fd1e314878c3b3e32b233d317355
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-12-14 09:45:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Searching an element in a sorted array (Ternary Search)/searching-an-element-in-a-sorted-array-ternary-search.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    // Function to find element in sorted array
+    // arr: input array
+    // N: size of array
+    // K: element to be searche
+    int ternarySearch(int arr[], int N, int K) 
+    { 
+       // Your code here
+       for(int i=0;i<N;i++)
+       {
+           if(arr[i]==K)
+           {
+               return 1;
+           }
+       }
+       return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main(void) 
+{ 
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int N, K;
+        cin >> N >> K;
+        
+        int arr[N];
+        
+        for(int i = 0;i<N;i++){
+            cin >> arr[i];
+        }
+        Solution ob;
+        cout << ob.ternarySearch(arr, N, K) << endl;
+
+    
+cout << "~" << "\n";
+}
+
+	return 0; 
+} 
+
+// } Driver Code Ends
