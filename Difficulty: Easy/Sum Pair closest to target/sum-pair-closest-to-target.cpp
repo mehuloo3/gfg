524d5b1ad63e7e23ceb1f3278043a95e70a7273b
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2025-01-06 11:01:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum Pair closest to target/sum-pair-closest-to-target.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    vector<int> sumClosest(vector<int>& arr, int target) {
+        // code here
+      sort(arr.begin(),arr.end());
+      int res=INT_MAX;
+      int ans=INT_MIN;
+      int n=arr.size();
+      int i=0;
+      int j=n-1;
+      vector<int>a;
+      while(i<j)
+      {
+          int x=arr[j]+arr[i];
+          int diff=abs(target-x);
+          int y=abs(arr[i]-arr[j]);
+          if(diff<res)
+          {
+              res=diff;
+              a={arr[i],arr[j]};
+          }
+          else if(x<=target)
+          {
+              i++;
+          }
+          else
+         {
+           j--;     
+          }
+      }
+      return a;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int target = stoi(ks);
+        Solution ob;
+        vector<int> ans = ob.sumClosest(arr, target);
+        if (ans.empty()) {
+            cout << "[]";
+        } else {
+            for (auto it : ans) {
+                cout << it << " ";
+            }
+        }
+        cout << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
