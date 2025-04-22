badd00ba9a0a77c22cd907fd4255d9bf88e1b44e
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2025-04-22 23:03:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Unique Number I/unique-number-i.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int findUnique(vector<int> &arr) {
+        // code here
+        int ans=0;
+        unordered_map<int,int>mp;
+        for(auto i:arr)
+        {
+            mp[i]++;
+        }
+        for(auto i:mp)
+        {
+            if(i.second<2)
+            {
+                ans=i.first;
+            }
+        }
+        return ans;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.findUnique(arr);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
