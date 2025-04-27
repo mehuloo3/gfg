73190d8ed3756098538c9b5d18e4fbd3f8e48b85
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2025-04-27 14:21:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Multiply two strings/multiply-two-strings.py
+++ Difficulty: Medium/Multiply two strings/multiply-two-strings.py
@@ -1,13 +1,13 @@
 #User function Template for python3
 
 class Solution:
-    def multiplyStrings(self,s1,s2):
+    def multiplyStrings(self, s1, s2):
         # code here
         # return the product string
-        n=int(s1)
-        m=int(s2)
-        return str(n*m)
-        
+        a=int(s1)
+        b=int(s2)
+        c=(a*b)
+        return str(c)
 
 
 #{ 
@@ -15,10 +15,12 @@ class Solution:
 #Initial Template for Python 3
 
 if __name__ == "__main__":
-    t=int(input())
+    t = int(input())
     for i in range(t):
-        a,b=input().split()
-        print(Solution().multiplyStrings( a.strip() , b.strip() ))
+        a = input()
+        b = input()
+        print(Solution().multiplyStrings(a.strip(), b.strip()))
 
         print("~")
+
 # } Driver Code Ends
